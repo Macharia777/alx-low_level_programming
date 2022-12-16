f2ef961cceb1bee0823b4c9f5c83f5569cc266e9
Macharia777 <gacheruarchibald@gmail.com>
2022-12-16 13:08:53 -0800

    hot fix 2

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers -  checks for checks for a digit 0123456789\n.
+ * print_numbers -  prints 0123456789\n.
  *@c: variable that checks
  * Return: Always 0.
  */
 void print_numbers(void)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	return(0);
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
